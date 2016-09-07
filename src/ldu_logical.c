0a562c51c766015bd251b2baa663890ec9d46059
joohyun kyong <joohyun0115@gmail.com>
2016-09-07 12:39:06 +0900

    update

--- src/ldu_logical.c
+++ src/ldu_logical.c
@@ -3,12 +3,12 @@ bool ldu_logical_insert(struct object_struct *obj,
 		void *head)
 {
 	...
-	// Phase 1 : update-side absorbing logs
+	// Phase 1 : update-side removing logs
 	// atomic swap due to synchronize update's logs
-	if(!xchg(&remove->mark, 0)){
-		BUG_ON(insert->mark);
-		// Phase 2 : reuse garbage log
+	if(!xchg(&obj->remove->mark, 0)){
+		BUG_ON(obj->insert->mark);
 		insert->mark = 1;
+		// Phase 2 : reuse garbage log
 		if(!test_and_set_bit(LDU_INSERT,
 				&obj->ldu.used)){
 			// Phase 3 : insert log to queue
@@ -24,16 +24,17 @@ bool ldu_logical_remove(struct object_struct *obj,
 {
 	...
 
-	// Phase 1 : update-side absorbing logs
+	// Phase 1 : update-side removing logs
 	// atomic swap due to synchronize update's logs
-	if(!xchg(&insert->mark, 0)){
-		BUG_ON(remove->mark);
-		// Phase 2 : reuse garbage log
+	if(!xchg(&obj->insert->mark, 0)){
+		BUG_ON(obj->remove->mark);
 		remove->mark = 1;
+		// Phase 2 : reuse garbage log
 		if(!test_and_set_bit(LDU_REMOVE,
 				&obj->ldu.used)){
 			// Phase 3 : insert log to queue
 			//...save argument and operation
+			ldu_insert_queue(root, insert);
 		}
 	}
 	...
