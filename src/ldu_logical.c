cdb4e4382696c679d399abcbbf06866d91b339d1
joohyun kyong <joohyun0115@gmail.com>
2016-10-15 22:12:32 +0900

    remove kor

--- src/ldu_logical.c
+++ src/ldu_logical.c
@@ -2,15 +2,14 @@
 bool ldu_logical_insert(struct object_struct *obj,
 		void *head) {
 	// Phase 1 : update-side removing logs
-	// atomic swap due to synchronize function
 	if (!SWAP(&obj->ldu.remove.mark, false)){
 		ASSERT(obj->ldu.insert.mark);
 		obj->ldu.insert.mark = true;
 		// Phase 2 : reuse garbage log
 		if (!TEST_AND_SET_BIT(LDU_INSERT,
 				&obj->ldu.used)){
-			// Phase 3 : insert log to queue
-			//...save argument and operation
+			// Phase 3(slow-path): insert log to queue
+			// ... : save argument and operation
 			ldu_insert_queue(root, insert);
 		}
 	}
@@ -19,15 +18,14 @@ bool ldu_logical_insert(struct object_struct *obj,
 bool ldu_logical_remove(struct object_struct *obj,
 		void *head) {
 	// Phase 1 : update-side removing logs
-	// atomic swap due to synchronize function
 	if (!SWAP(&obj->ldu.insert.mark, false)){
 		ASSERT(obj->ldu.remove.mark);
 		obj->ldu.remove.mark = true;
 		// Phase 2 : reuse garbage log
 		if (!TEST_AND_SET_BIT(LDU_REMOVE,
 				&obj->ldu.used)){
-			// Phase 3 : insert log to queue
-			//...save argument and operation
+			// Phase 3(slow-path): insert log to queue
+			//... : save argument and operation
 			ldu_insert_queue(root, insert);
 		}
 	}
--- src/ldu_physical.c
+++ src/ldu_physical.c
@@ -1,17 +1,16 @@
 
 void synchronize_ldu(struct obj_root *root)
 {
-	//atomic swap due to remove all logs
 	entry = SWAP(&head->first, NULL);
 	//iteration all logs
 	for_each_all_logs(log, entry, llist) {
-		//get log's arguments
+		//... : get log's arguments
 		//atomic swap due to update-side removing
 		if (SWAP(&log->mark, false))
-			ldu_physical_update(log->op_num, arg, log->root);
+			ldu_apply_log(log->op_num, arg, log->root);
 		CLEAR_BIT(log->op_num, &obj->ldu.used);
-		// one more check due to reuse garbage log
+		// once again check due to reusing garbage logs
 		if (SWAP(&log->mark, false))
-			ldu_physical_update(log->op_num, arg, log->root);
+			ldu_apply_log(log->op_num, arg, log->root);
 	}
 }
